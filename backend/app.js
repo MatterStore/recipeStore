c32edf04ad7cac16906849ef54c695bfcaab7420
Paul Hutchins <paul.hutchins@uptickhq.com>
2022-08-19 01:25:52 +1000

    Feat: Docker prod setup

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,16 @@
-require("dotenv").config({ path: `./.env.${process.env.NODE_ENV}` });
+require("dotenv").config({
+  path: `./.env.${process.env.NODE_ENV}`,
+});
 require("./config/db-connection");
 
 const express = require("express");
 const expressSession = require("express-session");
 const cors = require("cors");
+const passport = require("passport");
+
+const app = express();
+
+app.use(cors());
 app.use(express.json());
 app.use(
   expressSession({
@@ -15,9 +22,18 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 require("./config/passport")(passport);
+
 const users_route = require("./routes/user");
+
 app.use("/user", users_route);
+
+// default case for unmatched routes
+app.use(function (req, res) {
+  res.status(404);
+});
+
 const port = process.env.SERVER_PORT;
+
 app.listen(port, () => {
   console.log(`\nServer Started on ${port}`);
 });
