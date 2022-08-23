16adc3d18c6b50d155dd2dea74c646e8149d1348
Manu Masson <mmasson@student.unimelb.edu.au>
2022-08-23 16:01:28 +1000

    MS-34: use heroku env variables for atlas connection

--- backend/config/db-connection.js
+++ backend/config/db-connection.js
@@ -1,12 +1,9 @@
 const mongoose = require("mongoose");
 
-const DB_HOST = process.env.DB_HOST;
-const DB_PORT = process.env.DB_PORT;
-const DB_NAME = process.env.DB_NAME;
 const DB_USER = process.env.DB_USER;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 
-const DB_URL = "mongodb+srv://manu354:manu354@cluster0.jd0lpeu.mongodb.net/?retryWrites=true&w=majority";
+const DB_URL = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.jd0lpeu.mongodb.net/?retryWrites=true&w=majority`;
 console.log(DB_URL);
 
 const mongoOpts = {
