b5bf5598d1c22d5733ecefcd6e603956af9cbb25
Daniel B <dantheman41@live.com.au>
2022-08-23 23:01:18 +1000

    MS-22: Finalise button styles, layout landing

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -4,11 +4,11 @@ export default function Button(props) {
     return (
         <Link 
             to={props.to}
-            className={`py-3 px-6 m-4 text-lg text-white rounded ${
+            className={`py-3 px-6 m-8 ml-0 text-xl text-white rounded-md ${
                 props.primary ?
-                    `text-white bg-indigo-500`
+                    `text-white bg-indigo-600`
                 :
-                    `rounded-lg text-indigo-900 ring-inset ring-indigo-500 ring-4`
+                    `text-indigo-900 ring-inset ring-indigo-400 ring-2`
             }`}
         >
             {props.children}
--- frontend/src/containers/Home/Home.jsx
+++ frontend/src/containers/Home/Home.jsx
@@ -3,13 +3,24 @@ import Button from "../../components/Button";
 export default function Home() {
   return (
     <div>
-      <main>
-        <h1 className="text-5xl mb-10 subpixel-antialiased">Recipe Store App</h1>
+      <main className="min-h-screen w-screen flex flex-column items-center">
+        <div className="flex flex-row justify-around flex-grow">
+          <div className="p-8 self-center mb-32">
+            <h1 className="text-5xl mb-5 subpixel-antialiased">Recipe Store App</h1>
+            <nav className="flex">
+              <Button primary={true} to="/signup">Sign Up</Button>
+              <Button primary={false} to="/login">Login</Button>
+            </nav>
+          </div>
+          <div className="p-8">
+            <img
+              className="rounded-xl"
+              src="https://images.unsplash.com/photo-1466637574441-749b8f19452f?ixlib=rb-1.2.1&w=640&q=80&fm=jpg&crop=entropy&cs=tinysrgb"
+              alt="Food on bench"
+            />
+          </div>
+        </div>
       </main>
-      <nav>
-        <Button primary={true} to="/signup">Sign Up</Button>
-        <Button primary={false} to="/login">Login</Button>
-      </nav>
     </div>
   );
 }
