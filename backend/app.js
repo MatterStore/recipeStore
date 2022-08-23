535dac075f55c18705f15d6e52d24d0428a05519
manu354 <mmasson@student.unimelb.edu.au>
2022-08-23 15:56:22 +1000

    MS-34: set express static correctly

--- backend/app.js
+++ backend/app.js
@@ -26,10 +26,12 @@ require("./config/passport")(passport);
 
 
 // if (process.env.NODE_ENV === "production" || process.env.NODE_ENV === "staging") {
-  app.use(express.static("/../frontend/build"));
+  app.use(express.static("frontend/build"));
   app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/../frontend/build/index.html'));
-  });
+  const buildPath = path.join(__dirname + '/../frontend/build/index.html')
+  res.sendFile(buildPath);
+  console.log(buildPath)
+});
 //  }
 
 const users_route = require("./routes/user");
