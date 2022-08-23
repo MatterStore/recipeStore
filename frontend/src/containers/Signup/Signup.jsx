d03af02cef5bff49ba1547e216946a91d304533b
Daniel B <dantheman41@live.com.au>
2022-08-24 00:05:38 +1000

    MS-22: Make signup page (copy login page)

--- frontend/src/containers/Signup/Signup.jsx
+++ frontend/src/containers/Signup/Signup.jsx
@@ -4,11 +4,26 @@ import Textfield from "../../components/Textfield";
 
 export default function Signup() {
   return (
-    <div>
-      <Header>Signup Page</Header>
-      <Textfield></Textfield>
-      <Textfield></Textfield>
-      <Button to="/listing">Signup</Button>
+    <div className="flex row min-w-screen justify-center">
+      <main className="min-h-screen w-screen flex flex-column items-center max-w-screen-2xl">
+        <div className="flex flex-col md:flex-row justify-around flex-grow">
+          <div className="p-8 self-center md:mb-32">
+            <Header>Sign Up</Header>
+            <div className="flex flex-col min-w-full">
+              <Textfield params={{placeholder:"Email"}}>Email</Textfield>
+              <Textfield params={{placeholder:"Password", type: "password"}}>Password</Textfield>
+              <Button primary={true} to="/listing">Sign Up</Button>
+            </div>
+          </div>
+          <div className="p-8 self-center">
+            <img
+              className="rounded-xl self-center"
+              src="https://images.unsplash.com/photo-1466637574441-749b8f19452f?ixlib=rb-1.2.1&w=640&q=80&fm=jpg&crop=entropy&cs=tinysrgb"
+              alt="Food on bench"
+            />
+          </div>
+        </div>
+      </main>
     </div>
   );
 };
