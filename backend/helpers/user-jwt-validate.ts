c54627e20df5c6dc3c9d22271140fff10ca7650f
Manu Masson <mmasson@student.unimelb.edu.au>
2022-08-24 14:34:01 +1000

    MS-66: min changes to migrate to typescript

---
+++ backend/helpers/user-jwt-validate.ts
@@ -0,0 +1,14 @@
+const jwt = require("jsonwebtoken");
+
+module.exports.validateUserJWTToken = function (user_token) {
+  if (!user_token) return false;
+  if (user_token.length <= 4) return false;
+  let token = user_token.substr(4, user_token.length);
+  try {
+    const verified = jwt.verify(token, (process as any).send.JWT_SECRET);
+    if (!verified || !verified.data) return false;
+    return verified.data;
+  } catch (error) {
+    return false;
+  }
+};
---
+++ backend/models/user.ts
@@ -0,0 +1,74 @@
+const bcrypt = require("bcryptjs");
+
+const userSchema = mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  emailVerified: {
+    type: Boolean,
+    required: false,
+  },
+  admin: {
+    type: Boolean,
+    required: false,
+  },
+});
+
+const User = (module.exports = mongoose.model("User", userSchema));
+
+module.exports.getUserById = function (id, callback) {
+  User.findById({ _id: id }, callback);
+};
+
+module.exports.authenticateUser = function (email, callback) {
+  User.updateOne({ email: email }, { $set: { authenticated: true } }, callback);
+};
+
+module.exports.getUserByEmail = function (email, callback) {
+  const query = { email: email };
+  User.findOne(query, callback);
+};
+
+module.exports.addUser = function (newUser, callback) {
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(newUser.password, salt, (err, hash) => {
+      if (err) throw err;
+      newUser.password = hash;
+      newUser.save(callback);
+    });
+  });
+};
+
+module.exports.comparePassword = function (candidatePassword, hash, callback) {
+  if (!candidatePassword) {
+    return false;
+  }
+  bcrypt.compare(candidatePassword, hash, (err, isMatch) => {
+    if (err) throw err;
+    callback(null, isMatch);
+  });
+};
+
+module.exports.updatePassword = function (newUser, callback) {
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) throw err;
+    bcrypt.hash(newUser.newPassword, salt, (err, hash) => {
+      if (err) throw err;
+      newUser.newPassword = hash;
+      User.updateOne(
+        { email: newUser.email },
+        { $set: { password: newUser.newPassword } },
+        callback
+      );
+    });
+  });
+};
