f600201613f23bcae5ca5579e63b487faec6d128
manu354 <mmasson@student.unimelb.edu.au>
2022-08-26 14:00:19 +1000

    MS-66: improve TS best practices

--- backend/helpers/user-jwt-validate.ts
+++ backend/helpers/user-jwt-validate.ts
@@ -5,7 +5,7 @@ export function validateUserJWTToken (user_token) {
   if (user_token.length <= 4) return false;
   let token = user_token.substr(4, user_token.length);
   try {
-    const verified = jwt.verify(token, (process as any).send.JWT_SECRET);
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
     if (!verified || !verified.data) return false;
     return verified.data;
   } catch (error) {
--- backend/routes/recipe.ts
+++ backend/routes/recipe.ts
@@ -6,6 +6,8 @@ import * as params_validator from "../helpers/params-validator.js";
 
 import * as Recipe from "../models/recipe.js";
 
+import User from "../models/user.js";
+
 const router = express.Router();
 
 router.post(
