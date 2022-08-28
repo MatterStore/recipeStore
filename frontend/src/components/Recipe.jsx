e4ddc7706ad638279811e0b55db04ec58e7d403d
Daniel B <dantheman41@live.com.au>
2022-08-28 18:38:48 +1000

    MS-15: Basic recipe card and listing cleanup

--- frontend/src/components/Recipe.jsx
+++ frontend/src/components/Recipe.jsx
@@ -2,10 +2,11 @@ import { Link } from "react-router-dom";
 
 export default function Recipe(props) {
     return (
-        <div>
-            <Link to={`/recipe/${props.id}`}>
-                {props.name}
-            </Link>
-        </div>
+        <Link
+            to={`/recipe/${props.id}`}
+            className={`w-72 h-40 bg-gray-200 mr-4 mb-4 rounded p-8 inline-block text-xl`}
+        >
+            {props.name}
+        </Link>
     );
 }
