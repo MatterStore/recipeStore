f087e911f2a30e0f8a0e8f8aff31d3a506d51633
Daniel B <dantheman41@live.com.au>
2022-08-28 19:33:55 +1000

    MS-15: Final style tweaks and mobile support

--- frontend/src/components/Subheader.jsx
+++ frontend/src/components/Subheader.jsx
@@ -1,6 +1,6 @@
 export default function Subheader(props) {
     return (
-        <h3 className={`text-2xl mb-8 mt-8 md:mt-0 subpixel-antialiased`}>
+        <h3 className={`text-2xl mb-8 mt-2 subpixel-antialiased`}>
             {props.children}
         </h3>
     );
