c5591d99d1af0673f31dc5739cdd2c54af36e656
OwenFeik <owenhfeik@gmail.com>
2022-08-29 14:37:39 +1000

    MS-68: Add /user/login tests

--- backend/tests/user.test.ts
+++ backend/tests/user.test.ts
@@ -4,13 +4,15 @@ import { assert } from "chai";
 import app from '../app.js';
 import User from "../models/user.js";
 
-describe("POST /user/signup", () => {
-    before(() => User.deleteMany({}));
+// Clear users collection of test DB before and after tests
+before(() => User.deleteMany({}));
+after(() => User.deleteMany({}));
 
+describe("POST /user/signup", () => {
     it("Should register a new user", () => request(app)
         .post("/user/signup")
         .send({
-            email: "test@gmail.com",
+            email: "test@test.domain",
             name: "testuser",
             password: "testpassword"
         })
@@ -23,7 +25,7 @@ describe("POST /user/signup", () => {
     it("Should prevent duplicate emails", () => request(app)
         .post("/user/signup")
         .send({
-            email: "test@gmail.com",
+            email: "test@test.domain",
             name: "testuser",
             password: "testpassword"
         })
@@ -32,6 +34,42 @@ describe("POST /user/signup", () => {
             assert(res.status === 422, "Signup not error code.");
         })
     )
+});
 
-    after(() => User.deleteMany({}));
+describe("POST /user/login", () => {
+    it("Should fail for missing user", () => request(app)
+        .post("/user/login")
+        .send({
+            email: "missing@weird.domain",
+            password: "missinguserspassword"
+        })
+        .then((res) => {
+            assert(!res.body.success, "Missing user login succeeded.");
+            assert(res.status >= 400, "Incorrect status.");
+        })
+    );
+
+    it("Should fail for incorrect password", () => request(app)
+        .post("/user/login")
+        .send({
+            email: "test@test.domain",
+            password: "wrongpassword"
+        })
+        .then((res) => {
+            assert(!res.body.success, "Incorrect password allowed.");
+            assert(res.status >= 400, "Incorrect status.");
+        })
+    );
+
+    it("Should allow a valid login", () => request(app)
+        .post("/user/login")
+        .send({
+            email: "test@test.domain",
+            password: "testpassword"
+        })
+        .then((res) => {
+            assert(res.body.success, "Correct login rejected.");
+            assert(res.status == 200, "Status not 200 OK.");
+        })
+    );
 });
