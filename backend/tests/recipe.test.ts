ffa49e56ac1845d841b0f509e0d79a09266aa15e
OwenFeik <owenhfeik@gmail.com>
2022-08-29 18:16:50 +1000

    MS-68: Fix typos, add another test

--- backend/tests/recipe.test.ts
+++ backend/tests/recipe.test.ts
@@ -4,40 +4,53 @@ import { assert } from "chai";
 import app from '../app.js';
 import { Recipe } from "../models/recipe.js";
 
-var token: string | null = null;
 
-const TEST_USER_EMAIL = "chef@kitchen.table";
-const TEST_USER_PASSWORD = "recipesrgr8";
+interface UserDetails {
+    email: string,
+    password: string
+};
 
-// Log in as the test recipe user and sets up a test case with teh given
-// message, passing in an authorization token to the callback.
-function whenLoggedInIt(msg: string, cb: (token: string) => any) {
-    it(msg, () => request(app)
+const USER_A_DETAILS = {
+    email: "chef@kitchen.table",
+    name: "chef",
+    password: "recipesrgr8",
+};
+
+const USER_B_DETAILS = {
+    email: "beatrice@sushi.kitchen",
+    name: "beatrice",
+    password: "1<3fish4dinner"
+};
+
+async function doLoggedIn(
+    cb: (token: string) => any,
+    details: UserDetails = USER_A_DETAILS
+) {
+    await request(app)
         .post("/user/login")
-        .send({
-            email: TEST_USER_EMAIL,
-            password: TEST_USER_PASSWORD
-        })
+        .send(details)
         .then(res => {
             assert(res.body.success, "Login failed");
             assert(res.body.token.startsWith("JWT"), "Bad token.");
             cb(res.body.token);
-        }) 
-    );
+        });
+}
+
+// Log in as the test recipe user and sets up a test case with the given
+// message, passing in an authorization token to the callback.
+function whenLoggedInIt(
+    msg: string, cb: (token: string) => any, as: UserDetails = USER_A_DETAILS
+) {
+    it(msg, () => doLoggedIn(cb, as));
 }
 
 before(async () => {
     Recipe.deleteMany({}); // Clear recipes collection
 
-    // Create test user, just returns failure if they already exist, so no need
-    // to check for that.
-    await request(app)
-        .post("/user/signup")
-        .send({
-            email: TEST_USER_EMAIL,
-            name: "chef",
-            password: TEST_USER_PASSWORD
-        });
+    // Create test users, just returns failure if they already exist, so no
+    // need to check for that.
+    await request(app).post("/user/signup").send(USER_A_DETAILS);
+    await request(app).post("/user/signup").send(USER_B_DETAILS);
 });
 
 describe("POST /recipes/new", () => {
@@ -127,25 +140,47 @@ describe("GET /recipes/all", () => {
 });
 
 describe("GET /recipes/:id", async () => {
-    whenLoggedInIt("Should be able to get a recipe", token => request(app)
+    // Note; this is supposed to find a private recipe
+    let id = null;
+    before(async () => await doLoggedIn(token => request(app)
         .get("/recipes/all")
         .set("Authorization", token)
         .send()
         .then(res => {
             assert(res.body.list?.length >= 1, "Recipe not in list.");
-            let id = res.body.recipes[0].id;
-            request(app)
-                .get("/recipes/" + id)
-                .set("Authorization", token)
-                .send()
-                .then(res => {
-                    assert(res.body.success, "Failed to get recipe.");
-                    assert(
-                        res.body.recipe?.id == id,
-                        "Response didn't have recipe."
-                    );
-                    assert(res.status == 200, "Status not 200 OK.");
-                });
+            id = res.body.recipes[0].id;
+        })
+    ));
+
+    whenLoggedInIt("Should work author user", token => request(app)
+        .get("/recipes/" + id)
+        .set("Authorization", token)
+        .send()
+        .then(res => {
+            assert(res.body.success, "Failed to get recipe.");
+            assert(
+                res.body.recipe?.id == id,
+                "Response didn't have recipe."
+            );
+            assert(res.status == 200, "Status not 200 OK.");
         })
     );
-})
+
+    whenLoggedInIt("Shouldn't work for other user", token => request(app)
+        .get("/recipes/" + id)
+        .set("Authorization", token)
+        .send()
+        .then(res => {
+            assert(!res.body.success, "Was able to access private recipe.");
+            assert(res.status >= 400, "Status indicates success (shouldn't)");
+        })
+    );
+
+    it("Should not work if unauthenticated", () => request(app)
+        .get("/recipes/" + id)
+        .send()
+        .then(res => assert(
+            res.status == 401, "Allowed unauthenticated user."
+        ))
+    );
+});
