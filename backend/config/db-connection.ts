324cd3cf6fd7bd65c78b58dba7d8789326a1934e
OwenFeik <owenhfeik@gmail.com>
2022-08-29 18:34:12 +1000

    MS-68: Use test database

--- backend/config/db-connection.ts
+++ backend/config/db-connection.ts
@@ -1,19 +1,22 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv"
 
-dotenv.config({
-  path: `.env`,
-});
+dotenv.config({ path: `.env` });
 
 export default function connect() {
   const DB_USER = process.env.DB_USER;
   const DB_PASSWORD = process.env.DB_PASSWORD;
   
   const DB_HOST_PROD = "cluster0.jd0lpeu.mongodb.net";
-  const DB_HOST_TEST = "cluster0.jd0lpeu.mongodb.net";
-  const DB_HOST = process.env.NODE_ENV == "testing" ? DB_HOST_TEST : DB_HOST_PROD; 
+  const DB_HOST_TEST = "cluster0.rvya9b7.mongodb.net";
+  const DB_HOST = (
+    process.env.NODE_ENV == "testing" ? DB_HOST_TEST : DB_HOST_PROD
+  );
   
-  const DB_URL = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/?retryWrites=true&w=majority`;
+  const DB_URL = (
+    `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}`
+    + "/?retryWrites=true&w=majority"
+  );
   
   console.log(DB_URL);
   
