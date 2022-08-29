93841592f4135ce2ba2a158ce15c22aaa9db6a78
OwenFeik <owenhfeik@gmail.com>
2022-08-29 19:16:37 +1000

    MS-68: Use in-memory server for testing

--- backend/config/db-connection.ts
+++ backend/config/db-connection.ts
@@ -1,28 +1,33 @@
 import mongoose from "mongoose";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 
 dotenv.config({ path: `.env` });
-
-export default function connect() {
-  const DB_USER = process.env.DB_USER;
-  const DB_PASSWORD = process.env.DB_PASSWORD;
-  
-  const DB_HOST_PROD = "cluster0.jd0lpeu.mongodb.net";
-  const DB_HOST_TEST = "cluster0.rvya9b7.mongodb.net";
-  const DB_HOST = (
-    process.env.NODE_ENV == "testing" ? DB_HOST_TEST : DB_HOST_PROD
-  );
-  
-  const DB_URL = (
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_HOST = (
+    process.env.NODE_ENV == "testing"
+    ? "cluster0.rvya9b7.mongodb.net"
+    : "cluster0.jd0lpeu.mongodb.net"
+);
+const DB_URL = (
     `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}`
     + "/?retryWrites=true&w=majority"
-  );
-  
-  console.log(DB_URL);
-  
-  const mongoOpts : any = {
+);
+
+const mongoOpts: any = {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  };
-  mongoose.connect(DB_URL, mongoOpts);  
+};
+
+import { MongoMemoryServer } from 'mongodb-memory-server';
+
+if (process.env.NODE_ENV == "testing") {
+    MongoMemoryServer.create().then(
+        (mongodb) => mongoose.connect(mongodb.getUri(), mongoOpts)
+    );
 }
+else {
+    mongoose.connect(DB_URL, mongoOpts);
+}
+
+export default {};
