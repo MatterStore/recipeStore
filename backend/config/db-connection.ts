8506815eb1e8af9c4414834b2dcb9e52dc809f0f
OwenFeik <owenhfeik@gmail.com>
2022-08-30 13:28:27 +1000

    MS-68: Revert change to in-memory server, will put in different PR
    This reverts commit 13f3a29eb9f86cfbd27833423e48df9b1ddb4fe3.

--- backend/config/db-connection.ts
+++ backend/config/db-connection.ts
@@ -1,33 +1,28 @@
 import mongoose from "mongoose";
-import dotenv from "dotenv";
+import dotenv from "dotenv"
 
 dotenv.config({ path: `.env` });
-const DB_USER = process.env.DB_USER;
-const DB_PASSWORD = process.env.DB_PASSWORD;
-const DB_HOST = (
-    process.env.NODE_ENV == "testing"
-    ? "cluster0.rvya9b7.mongodb.net"
-    : "cluster0.jd0lpeu.mongodb.net"
-);
-const DB_URL = (
+
+export default function connect() {
+  const DB_USER = process.env.DB_USER;
+  const DB_PASSWORD = process.env.DB_PASSWORD;
+  
+  const DB_HOST_PROD = "cluster0.jd0lpeu.mongodb.net";
+  const DB_HOST_TEST = "cluster0.rvya9b7.mongodb.net";
+  const DB_HOST = (
+    process.env.NODE_ENV == "testing" ? DB_HOST_TEST : DB_HOST_PROD
+  );
+  
+  const DB_URL = (
     `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}`
     + "/?retryWrites=true&w=majority"
-);
-
-const mongoOpts: any = {
+  );
+  
+  console.log(DB_URL);
+  
+  const mongoOpts : any = {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-};
-
-import { MongoMemoryServer } from 'mongodb-memory-server';
-
-if (process.env.NODE_ENV == "testing") {
-    MongoMemoryServer.create().then(
-        (mongodb) => mongoose.connect(mongodb.getUri(), mongoOpts)
-    );
+  };
+  mongoose.connect(DB_URL, mongoOpts);  
 }
-else {
-    mongoose.connect(DB_URL, mongoOpts);
-}
-
-export default {};
