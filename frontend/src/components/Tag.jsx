b20150a0cafea550e79b2cf879f1045952f4a560
Daniel B <dantheman41@live.com.au>
2022-09-01 20:19:22 +1000

    MS-16: Clean up tag and edit button positioning

--- frontend/src/components/Tag.jsx
+++ frontend/src/components/Tag.jsx
@@ -1,7 +1,7 @@
 export default function Tag(props) {
     return (
         <span 
-            className={`align-baseline py-1 px-3 m-8 ml-0 text-md text-slate-900 text-center rounded-full whitespace-nowrap max-w-fit $text-white bg-amber-400`}
+            className={`py-1.5 px-4 my-8 mr-4 text-md text-slate-900 text-center rounded-full whitespace-nowrap max-w-fit bg-amber-400`}
         >
             {props.children}
         </span>
