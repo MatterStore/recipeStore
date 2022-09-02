f97443f8b4a42fe89658913b330cbb9bcebc9a38
Paul Hutchins <paul.hutchins@uptickhq.com>
2022-09-02 18:26:09 +1000

    Feat: Change some colours, orange / blue

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -6,7 +6,7 @@ export default function Button(props) {
       to={props.to}
       className={`py-3 px-6 m-8 ml-0 text-xl text-white text-center rounded-md whitespace-nowrap max-w-fit ${
         props.primary
-          ? `text-white bg-indigo-600`
+          ? `text-white bg-orange-400 hover:bg-orange-500`
           : `text-indigo-900 ring-inset ring-indigo-400 ring-2`
       } ${props.className}`}
     >
--- frontend/src/components/Textfield.jsx
+++ frontend/src/components/Textfield.jsx
@@ -1,14 +1,12 @@
 export default function Textfield(props) {
-    return (
-        <label>
-            <div className="font-bold text-slate-600">
-                {props.children}
-            </div>
-            <input
-                type="text"
-                className="py-3 px-6 mr-8 mb-4 w-full text-xl rounded-md whitespace-nowrap text-indigo-900 ring-inset ring-indigo-400 ring-2"
-                {...props.params}
-            />
-        </label>
-    );
+  return (
+    <label>
+      <div className="font-bold text-slate-600">{props.children}</div>
+      <input
+        type="text"
+        className="py-3 px-6 mr-8 mb-4 w-full text-xl rounded-md whitespace-nowrap text-indigo-900 ring-inset ring-blue-400 ring-2"
+        {...props.params}
+      />
+    </label>
+  );
 }
