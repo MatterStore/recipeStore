581444b0d7fb4afc382a455db174c6879b2d2724
Paul Hutchins <paul.hutchins@uptickhq.com>
2022-09-08 22:50:25 +1000

    Feat(MS-82): Initial scaffolding of list view of recipes

--- frontend/src/containers/Listing/Listing.jsx
+++ frontend/src/containers/Listing/Listing.jsx
@@ -1,24 +1,43 @@
+import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
+
+import { AuthContext } from "../../contexts/AuthContext";
+import { usersRecipesRoute } from "../../api/routes";
+
 import Header from "../../components/Header";
 import Button from "../../components/Button";
 import Recipe from "../../components/Recipe";
 import Subheader from "../../components/Subheader";
 
 export default function Listing() {
-  const recipes = [
-    {
-      id: "1",
-      name: "Gnocchi",
-      primaryImage:
-        "https://unsplash.com/photos/Zmhi-OMDVbw/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8MXx8Z25vY2NoaXxlbnwwfHx8fDE2NjE2NzU5NjQ&w=400",
-    },
-    {
-      id: "2",
-      name: "Ratatouille",
-      primaryImage:
-        "https://unsplash.com/photos/3vDJ--i7w88/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8M3x8cmF0YXRvdWlsbGV8ZW58MHx8fHwxNjYxNjc2MDY0&w=400",
-    },
-  ];
+  const [CollectionsData, setCollectionsData] = useState(null);
+  const [CollectionsLoading, setCollectionsLoading] = useState(true);
+  const [CollectionsError, setCollectionsError] = useState(null);
+
+  const [recipesData, setRecipesData] = useState(null);
+  const [recipesLoading, setRecipesLoading] = useState(true);
+  const [recipesError, setRecipesError] = useState(null);
+
+  const { user } = useContext(AuthContext);
+
+  const recipes = fetch(usersRecipesRoute)
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+  // const recipes = [
+  //   {
+  //     id: "1",
+  //     name: "Gnocchi",
+  //     primaryImage:
+  //       "https://unsplash.com/photos/Zmhi-OMDVbw/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8MXx8Z25vY2NoaXxlbnwwfHx8fDE2NjE2NzU5NjQ&w=400",
+  //   },
+  //   {
+  //     id: "2",
+  //     name: "Ratatouille",
+  //     primaryImage:
+  //       "https://unsplash.com/photos/3vDJ--i7w88/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8M3x8cmF0YXRvdWlsbGV8ZW58MHx8fHwxNjYxNjc2MDY0&w=400",
+  //   },
+  // ];
+
   const collections = {
     "Your Recipe Collection": [1, 2],
     "Easy Dinners": [1],
@@ -53,7 +72,7 @@ export default function Listing() {
                 </Subheader>
                 <div className="self-center lg:self-start">
                   {collectionRecipes
-                    .map((index) => recipes[index - 1])
+                    .map((index) => recipesData[index - 1])
                     .map((recipe, j) => {
                       return (
                         <div
