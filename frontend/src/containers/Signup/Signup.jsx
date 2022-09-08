b3cdfca1521f7e0a34eb836710d18e7435ba8dd8
Paul Hutchins <paul.hutchins@uptickhq.com>
2022-09-08 23:05:39 +1000

    Feat(MS-86): Everything neccesary to create an account

--- frontend/src/containers/Signup/Signup.jsx
+++ frontend/src/containers/Signup/Signup.jsx
@@ -1,19 +1,129 @@
-import Button from "../../components/Button";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import validator from "validator";
+import axios from "../../api/axios";
+
+import { signupRoute } from "../../api/routes";
+
+import SubmitButton from "../../components/SubmitButton";
 import Header from "../../components/Header";
-import Textfield from "../../components/Textfield";
+import Formfield from "../../components/Formfield";
 
 export default function Signup() {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [nameError, setNameError] = useState("");
+  const [emailError, setemailError] = useState("");
+  const [passwordError, setpasswordError] = useState("");
+  const [signupError, setSignupError] = useState("");
+
+  const [formValid, setFormValid] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleValidation = (event) => {
+    let formIsValid = true;
+
+    if (!validator.isAlpha(name)) {
+      formIsValid = false;
+      setNameError("Name is Not Valid");
+      return false;
+    } else {
+      setNameError("");
+      formIsValid = true;
+    }
+
+    if (!validator.isEmail(email)) {
+      formIsValid = false;
+      setemailError("Email is Not Valid");
+      return false;
+    } else {
+      setemailError("");
+      formIsValid = true;
+    }
+
+    if (
+      !validator.isStrongPassword(password, {
+        minLength: 8,
+        minLowercase: 0,
+        minUppercase: 0,
+        minNumbers: 0,
+        minSymbols: 0,
+      })
+    ) {
+      formIsValid = false;
+      setpasswordError("Must contain 8 characters");
+      return false;
+    } else {
+      setpasswordError("");
+      formIsValid = true;
+    }
+    setFormValid(formIsValid);
+  };
+
+  const loginSubmit = (e) => {
+    e.preventDefault();
+    handleValidation();
+
+    if (formValid) {
+      axios
+        .post(signupRoute, {
+          name: name,
+          email: email,
+          password: password,
+        })
+        .then(function (response) {
+          navigate("/login");
+        })
+        .catch(function (error) {
+          setSignupError(error.response.data.message);
+        });
+    }
+  };
+
   return (
     <div className="flex row min-w-screen justify-center">
       <main className="min-h-screen w-screen flex flex-column items-center max-w-screen-2xl">
         <div className="flex flex-col md:flex-row justify-around flex-grow">
           <div className="p-8 self-center md:mb-32">
-            <Header>Sign Up</Header>
-            <div className="flex flex-col min-w-full">
-              <Textfield params={{placeholder:"Email"}}>Email</Textfield>
-              <Textfield params={{placeholder:"Password", type: "password"}}>Password</Textfield>
-              <Button primary={true} to="/listing">Sign Up</Button>
-            </div>
+            <form id="loginform" onSubmit={loginSubmit} className="login-form">
+              <Header>Sign Up</Header>
+              <div className="flex flex-col min-w-full">
+                <Formfield
+                  type="name"
+                  placeholder="Enter Name"
+                  setFunc={setName}
+                />
+                <small id="nameHelp" className="text-danger form-text mt-2">
+                  {nameError}
+                </small>
+                <Formfield
+                  type="email"
+                  placeholder="Enter Email"
+                  setFunc={setEmail}
+                />
+                <small id="emailHelp" className="text-danger form-text mt-2">
+                  {emailError}
+                </small>
+                <Formfield
+                  type="password"
+                  placeholder="Enter Password"
+                  setFunc={setPassword}
+                />
+                <small id="passwordError" className="text-danger form-text">
+                  {passwordError}
+                </small>
+                <SubmitButton primary={true} type="submit">
+                  Sign Up
+                </SubmitButton>
+                <small id="signupError" className="text-danger form-text">
+                  {signupError}
+                </small>
+              </div>
+            </form>
           </div>
           <div className="p-8 self-center">
             <img
@@ -26,4 +136,4 @@ export default function Signup() {
       </main>
     </div>
   );
-};
+}
