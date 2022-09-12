995fb7a84f2ea02c7316b0bf14505a52696d15a7
Paul Hutchins <paul.hutchins@uptickhq.com>
2022-09-12 16:15:09 +1000

    Fix: I lied, actually fixed spacing issues

--- frontend/src/components/Formfield.jsx
+++ frontend/src/components/Formfield.jsx
@@ -1,10 +1,10 @@
 export default function Formfield(props) {
   const { type, placeholder, setFunc } = props;
   return (
-    <label>
+    <label className="mt-2 mb-2">
       <div className="font-bold text-slate-600">{props.children}</div>
       <input
-        className="form-control py-3 px-6 mr-8 mb-4 w-full text-xl rounded-md whitespace-nowrap text-indigo-900 ring-inset ring-indigo-400 ring-2"
+        className="form-control py-3 px-6 mr-8 w-full text-xl rounded-md whitespace-nowrap text-indigo-900 ring-inset ring-indigo-400 ring-2"
         type={type}
         id={`${type}Input`}
         name={`${type}Input`}
