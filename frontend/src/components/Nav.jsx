6f792eece81a90d10cd0ec5e8cb3629ea204bc0b
Manu Masson <mmasson@student.unimelb.edu.au>
2022-09-17 21:53:23 +1000

    MS-89: fix styles

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -13,14 +13,14 @@ export default function Nav(props) {
         return (
             <nav class="py-2.5 bg-blue-900 fixed w-full z-20 top-0 left-0 border-b border-blue-600">
                 <div class="container flex flex-wrap justify-between items-center mx-auto">
-                        <ul class="flex p-4 mt-4 bg-blue-50 flex-row space-x-8 mt-0 text-sm font-medium border-0 bg-blue-900">
+                        <ul class="flex p-4 mt-4 flex-row space-x-8 mt-0 text-sm font-medium border-0 bg-blue-900">
                             {
                                 navigationRoutes.map(([title, path]) =>
                                     <li>
                                         <a href={`${path}`}
                                             class={`${location.pathname === path
                                                 ? `block py-2 pr-4 pl-3 rounded bg-transparent text-blue-700 p-0 `
-                                                : `block py-2 pr-4 pl-3 rounded p-0 hover:text-white text-blue-400 `
+                                                : `block py-2 pr-4 pl-3 text-blue-700 rounded hover:text-blue-700 p-0 dark:hover:text-white text-blue-400 `
                                                 }`}
                                             aria-current="page">{title}
                                         </a>
