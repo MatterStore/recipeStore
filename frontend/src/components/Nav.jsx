fa057df004aa0585d7cbb29fd372aed2e9556c16
Manu Masson <mmasson@student.unimelb.edu.au>
2022-09-17 21:45:31 +1000

    MS-89: simplyify styles

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -11,17 +11,16 @@ export default function Nav(props) {
 
     if (!doNotDisplayNavOn.includes(location.pathname)) {
         return (
-            <nav class="bg-white px-2 sm:px-4 py-2.5 dark:bg-blue-900 fixed w-full z-20 top-0 left-0 border-b border-blue-200 dark:border-blue-600">
+            <nav class="py-2.5 bg-blue-900 fixed w-full z-20 top-0 left-0 border-b border-blue-600">
                 <div class="container flex flex-wrap justify-between items-center mx-auto">
-                    <div class="hidden justify-between items-center w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
-                        <ul class="flex flex-col p-4 mt-4 bg-blue-50 rounded-lg border border-blue-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-blue-800 md:dark:bg-blue-900 dark:border-blue-700">
+                        <ul class="flex p-4 mt-4 bg-blue-50 flex-row space-x-8 mt-0 text-sm font-medium border-0 bg-blue-900">
                             {
                                 navigationRoutes.map(([title, path]) =>
                                     <li>
                                         <a href={`${path}`}
                                             class={`${location.pathname == path
-                                                ? `block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white`
-                                                : `block py-2 pr-4 pl-3 text-blue-700 rounded hover:bg-blue-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-blue-400 dark:hover:bg-blue-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-blue-700`
+                                                ? `block py-2 pr-4 pl-3 rounded bg-transparent text-blue-700 p-0 `
+                                                : `block py-2 pr-4 pl-3 rounded p-0 hover:text-white text-blue-400 `
                                                 }`}
                                             aria-current="page">{title}
                                         </a>
@@ -30,7 +29,6 @@ export default function Nav(props) {
                             }
                         </ul>
                     </div>
-                </div>
             </nav>
         )
     }
