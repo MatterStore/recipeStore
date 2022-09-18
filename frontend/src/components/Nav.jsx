9844e700dea2eab503f31eaf80e7685799491e7b
Manu Masson <mmasson@student.unimelb.edu.au>
2022-09-18 12:22:28 +1000

    MS-89: improve styles

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -11,18 +11,18 @@ export default function Nav(props) {
 
     if (!doNotDisplayNavOn.includes(location.pathname)) {
         return (
-            <nav class="py-2.5 bg-blue-900 fixed w-full z-20 top-0 left-0 border-b border-blue-600">
-                <div class="container flex flex-wrap justify-between items-center mx-auto">
-                        <ul class="flex p-4 mt-4 flex-row space-x-8 mt-0 text-sm font-medium border-0 bg-blue-900">
+            <nav className="py-1 bg-blue-100 fixed w-full z-20 top-0 left-0 border-b border-blue-100">
+                <div className="container flex flex-wrap justify-between items-center mx-auto">
+                    <p>StoreMyRecipe </p>
+                        <ul className="flex p-2 mt-4 flex-row space-x-8 mt-0 text-sm font-medium border-0 bg-blue-100">
                             {
                                 navigationRoutes.map(([title, path]) =>
                                     <li>
                                         <a href={`${path}`}
-                                            class={`${location.pathname === path
-                                                ? `block py-2 pr-4 pl-3 rounded bg-transparent text-blue-700 p-0 `
-                                                : `block py-2 pr-4 pl-3 text-blue-700 rounded hover:text-blue-700 p-0 dark:hover:text-white text-blue-400 `
-                                                }`}
-                                            aria-current="page">{title}
+                                            className={`${location.pathname === path
+                                                ? `block py-2 pr-4 pl-3 rounded bg-blue-200 text-blue-500 p-0 `
+                                                : `block py-2 pr-4 pl-3 text-blue-700 rounded hover:text-blue-700 p-0 dark:hover:text-white text-neutral-600 `
+                                                }`}>{title}
                                         </a>
                                     </li>
                                 )
