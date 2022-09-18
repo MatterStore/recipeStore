2741800160bf7cf02660244bddd27c47d7294fbf
Daniel B <dantheman41@live.com.au>
2022-09-18 13:29:32 +1000

    MS-89: Pad to align, tweak colour, container width

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -11,24 +11,25 @@ export default function Nav(props) {
 
     if (!doNotDisplayNavOn.includes(location.pathname)) {
         return (
-            <nav className="py-1 bg-blue-100 fixed w-full z-20 top-0 left-0 border-b border-blue-100">
-                <div className="container flex flex-wrap justify-between items-center mx-auto">
-                    <p>StoreMyRecipe </p>
-                        <ul className="flex p-2 mt-4 flex-row space-x-8 mt-0 text-sm font-medium border-0 bg-blue-100">
-                            {
-                                navigationRoutes.map(([title, path]) =>
-                                    <li key={path}>
-                                        <a href={`${path}`}
-                                            className={`${location.pathname === path
-                                                ? `block py-2 pr-4 pl-3 rounded bg-blue-200 text-blue-500 p-0 `
-                                                : `block py-2 pr-4 pl-3 text-blue-700 rounded hover:text-blue-700 p-0 dark:hover:text-white text-neutral-600 `
-                                                }`}>{title}
-                                        </a>
-                                    </li>
-                                )
-                            }
-                        </ul>
-                    </div>
+            <nav className="py-1 bg-blue-100 fixed w-full z-20">
+                <div className="px-16 p-2 flex flex-wrap container justify-between items-center mx-auto">
+                    <p className="cursor-default select-none">StoreMyRecipe </p>
+                    <ul className="flex flex-row space-x-8 text-sm">
+                        {
+                            navigationRoutes.map(([title, path]) =>
+                                <li key={path}>
+                                    <a
+                                        href={`${path}`}
+                                        className={`block py-2 px-2.5 rounded select-none transition-colors ${location.pathname === path
+                                            ? `bg-blue-200 text-blue-600 p-0 `
+                                            : `hover:text-blue-700 p-0 text-neutral-600 `
+                                            }`}>{title}
+                                    </a>
+                                </li>
+                            )
+                        }
+                    </ul>
+                </div>
             </nav>
         )
     }
