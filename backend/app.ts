a137a9cd934b6961bf014d20e9bbec4b64b1e298
Manu Masson <mmasson@student.unimelb.edu.au>
2022-09-20 20:27:52 +1000

    MS-93: fix deployments by ensuring __dirname is defined

--- backend/app.ts
+++ backend/app.ts
@@ -1,15 +1,15 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 const result = dotenv.config({
   path: `.env`,
 });
 
 import connect from "./config/db-connection.js";
-import path from 'path';
+import path from "path";
 import express from "express";
 import expressSession from "express-session";
 import cors from "cors";
 import passport from "passport";
-
+import { fileURLToPath } from "url";
 import * as users_route from "./routes/user.js";
 import collectionsRoute from "./routes/collection.js";
 import recipesRoute from "./routes/recipe.js";
@@ -30,22 +30,26 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 import execPassport from "./config/passport.js";
-execPassport(passport)
+execPassport(passport);
 
-if (process.env.NODE_ENV === "production" || process.env.NODE_ENV === "staging") {
-  app.use(express.static("frontend/build"));
-  app.get('*', (req, res) => {
-    const buildPath = path.join(__dirname + '/../frontend/build/index.html')
+const __filename = fileURLToPath(import.meta.url + "/../../");
+const __dirname = path.dirname(__filename);
+console.log(__dirname);
+if (
+  process.env.NODE_ENV === "production" ||
+  process.env.NODE_ENV === "staging"
+) {
+  app.use(express.static(path.resolve(__dirname, "frontend/build")));
+  app.get("*", (req, res) => {
+    const buildPath = path.join(path.resolve(__dirname, "frontend/build"));
     res.sendFile(buildPath);
-    console.log(buildPath)
+    console.log(buildPath);
   });
 }
-
 
 app.use("/user", users_route.router);
 app.use("/collections", collectionsRoute);
 app.use("/recipes", recipesRoute);
-
 
 // default case for unmatched routes
 app.use(function (req, res) {
