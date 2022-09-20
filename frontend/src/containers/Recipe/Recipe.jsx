38c5b2d6e68dcb921494abf85fc23c2945369518
jadesiang <81812345+jadesiang@users.noreply.github.com>
2022-09-20 22:33:59 +1000

    feat(MS-81): implemented axios to fetch data for single recipe view

--- frontend/src/containers/Recipe/Recipe.jsx
+++ frontend/src/containers/Recipe/Recipe.jsx
@@ -1,48 +1,41 @@
+import { useState } from "react";
 import { useParams } from "react-router-dom";
+
+import axios from "../../api/axios";
+
 import Header from "../../components/Header";
 import Subheader from "../../components/Subheader";
 import Button from "../../components/Button";
 import Tag from "../../components/Tag";
+import { recipeRoute } from "../../api/routes";
 
 export default function Recipe(props) {
+  const [recipeData, setRecipeData] = useState(null); 
+  const [recipeLoading, setRecipeLoading] = useState(true); 
+  const [recipeError, setRecipeError] = useState(null); 
+
   let params = useParams();
 
-  const recipes = [
-    {
-      id: "1",
-      name: "Gnocchi",
-      primaryImage:
-        "https://unsplash.com/photos/Zmhi-OMDVbw/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8MXx8Z25vY2NoaXxlbnwwfHx8fDE2NjE2NzU5NjQ&w=600",
-      ingredients: ["100g Flour", "1tspn Sugar"],
-      steps: ["Mix the flour with the sugar", "Serve and enjoy!"],
-      tags: ["Vegetarian"],
-      time: "3 hours",
-      servings: 4,
-    },
-    {
-      id: "2",
-      name: "Ratatouille",
-      primaryImage:
-        "https://unsplash.com/photos/3vDJ--i7w88/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8M3x8cmF0YXRvdWlsbGV8ZW58MHx8fHwxNjYxNjc2MDY0&w=600",
-      ingredients: ["250g Pumpkin", "500mL Water"],
-      steps: ["Pour the water onto the pumpkin", "Serve and enjoy!"],
-      tags: ["Anton Ego Approved", "Gluten Free"],
-      time: "15 minutes",
-      servings: 8,
-    },
-  ];
-
-  const getRecipe = (recipeId) => {
-    return recipes[recipeId - 1];
-  };
-
-  const recipe = getRecipe(params.recipeId);
+  useEffect(() => {
+    async function fetchRecipe() {
+      axios
+        .get(recipeRoute(params.recipeId))
+        .then((response) => {
+          setRecipeData(response.data);
+          setRecipeLoading(false);
+        })
+        .catch((error) => setRecipeError(error));
+    }
+    fetchRecipe();
+  }, []);
 
   return (
+    (!recipeLoading && !recipeError && (
+    
     <div className="px-16 lg:px-32 py-16 mx-auto container">
-      <Header inline>{recipe.name}</Header>
+      <Header inline>{recipeData.title}</Header>
       <span>
-        {recipe.time} — Serves {recipe.servings}
+        {recipeData.time} — Serves {recipeData.servings}
       </span>
       <span className="float-right">
         <Button primary={false} to="edit" className="leading-3">
@@ -50,7 +43,7 @@ export default function Recipe(props) {
         </Button>
       </span>
       <span className="block mt-4 lg:mt-0 lg:inline lg:float-right">
-        {recipe.tags.map((tag, i) => (
+        {recipeData.tags.map((tag, i) => (
           <Tag key={i}>{tag}</Tag>
         ))}
       </span>
@@ -61,7 +54,7 @@ export default function Recipe(props) {
           <section className="mb-12">
             <h3 className="text-xl font-bold mb-4">Ingredients</h3>
             <ul className="list-disc ml-5 leading-relaxed text-xl">
-              {recipe.ingredients.map((ingredient, i) => (
+              {recipeData.ingredients.map((ingredient, i) => (
                 <li key={i}>{ingredient}</li>
               ))}
             </ul>
@@ -69,7 +62,7 @@ export default function Recipe(props) {
           <section className="mb-12">
             <h3 className="text-xl font-bold mb-4">Steps</h3>
             <ol className="list-decimal ml-5 leading-relaxed text-xl">
-              {recipe.steps.map((step, i) => (
+              {recipeData.steps.map((step, i) => (
                 <li key={i}>{step}</li>
               ))}
             </ol>
@@ -77,12 +70,13 @@ export default function Recipe(props) {
         </article>
         <div>
           <img
-            src={recipe.primaryImage}
+            src={recipeData.primaryImage}
             alt=""
             className="object-cover w-full max-h-96 pb-12 mt-4 rounded"
           />
         </div>
       </main>
     </div>
+    ))
   );
 }
