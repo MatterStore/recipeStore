bea9e9e0687d3248c5ba48aa23673daf4c727fb2
Paul Hutchins <paul.hutchins@uptickhq.com>
2022-09-25 18:10:41 +1000

    Fix: Fix all eslint issues

--- backend/config/db-connection.ts
+++ backend/config/db-connection.ts
@@ -1,28 +1,25 @@
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 dotenv.config({ path: `.env` });
 
-import mongoose from "mongoose";
-import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from 'mongoose';
+import { MongoMemoryServer } from 'mongodb-memory-server';
 
 const DB_USER = process.env.DB_USER;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 const DB_HOST =
-  process.env.NODE_ENV == "testing"
-    ? "cluster0.rvya9b7.mongodb.net"
-    : "cluster0.jd0lpeu.mongodb.net";
+  process.env.NODE_ENV == 'testing'
+    ? 'cluster0.rvya9b7.mongodb.net'
+    : 'cluster0.jd0lpeu.mongodb.net';
 const DB_URL =
-  `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}` +
-  "/?retryWrites=true&w=majority";
+  `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}` + '/?retryWrites=true&w=majority';
 
-const mongoOpts: any = {
+const mongoOpts: unknown = {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
+  useUnifiedTopology: true
 };
 
-if (process.env.NODE_ENV == "testing") {
-  MongoMemoryServer.create().then((mongodb) =>
-    mongoose.connect(mongodb.getUri(), mongoOpts)
-  );
+if (process.env.NODE_ENV == 'testing') {
+  MongoMemoryServer.create().then((mongodb) => mongoose.connect(mongodb.getUri(), mongoOpts));
 } else {
   mongoose.connect(DB_URL, mongoOpts);
 }
--- backend/tests/collection.test.ts
+++ backend/tests/collection.test.ts
@@ -1,8 +1,8 @@
-import request from "supertest";
-import { assert } from "chai";
+import request from 'supertest';
+import { assert } from 'chai';
 
-import app from "../app.js";
-import Collection from "../models/collection.js";
+import app from '../app.js';
+import Collection from '../models/collection.js';
 import {
   assertFailed,
   assertSucceeded,
@@ -11,10 +11,10 @@ import {
   TestCollections,
   TestRecipes,
   TestUsers,
-  whenLoggedInIt,
-} from "./helpers.js";
+  whenLoggedInIt
+} from './helpers.js';
 
-const FLAG_TAG = "TESTCOLLECTION";
+const FLAG_TAG = 'TESTCOLLECTION';
 
 before(async () => {
   await Collection.deleteMany({ tags: FLAG_TAG });
@@ -23,76 +23,71 @@ before(async () => {
 
 after(() => Collection.deleteMany({ tags: FLAG_TAG }));
 
-describe("POST /collections/new", () => {
-  let collection = Object.assign({}, TestCollections.Breakfast);
+describe('POST /collections/new', () => {
+  const collection = Object.assign({}, TestCollections.Breakfast);
   collection.tags = collection.tags.concat([FLAG_TAG]);
 
-  itShouldRequireAuthentication("/collections/new", "post", collection);
+  itShouldRequireAuthentication('/collections/new', 'post', collection);
 
-  whenLoggedInIt("Should reject invalid object IDs", async (token) => {
-    let badCollection = Object.assign({}, collection);
-    badCollection.recipes = badCollection.recipes.concat([
-      "An Invalid ObjectID",
-    ]);
+  whenLoggedInIt('Should reject invalid object IDs', async (token) => {
+    const badCollection = Object.assign({}, collection);
+    badCollection.recipes = badCollection.recipes.concat(['An Invalid ObjectID']);
 
     await request(app)
-      .post("/collections/new")
-      .set("Authorization", token)
+      .post('/collections/new')
+      .set('Authorization', token)
       .send(badCollection)
-      .then((res) => assertFailed(res, "Invalid recipe ID allowed."));
+      .then((res) => assertFailed(res, 'Invalid recipe ID allowed.'));
   });
 
-  whenLoggedInIt("Should allow collection creation", (token) =>
+  whenLoggedInIt('Should allow collection creation', (token) =>
     request(app)
-      .post("/collections/new")
-      .set("Authorization", token)
+      .post('/collections/new')
+      .set('Authorization', token)
       .send(collection)
-      .then((res) => assertSucceeded(res, "Failed to create collection."))
+      .then((res) => assertSucceeded(res, 'Failed to create collection.'))
   );
 });
 
-describe("GET /collections/all", () => {
-  itShouldRequireAuthentication("/collections/all", "get");
+describe('GET /collections/all', () => {
+  itShouldRequireAuthentication('/collections/all', 'get');
 
-  whenLoggedInIt("Should return a list of collections", (token) =>
+  whenLoggedInIt('Should return a list of collections', (token) =>
     request(app)
-      .get("/collections/all")
-      .set("Authorization", token)
+      .get('/collections/all')
+      .set('Authorization', token)
       .send()
       .then((res) => {
-        assertSucceeded(res, "Failed to list collections.");
-        assert(res.body.list.length >= 1, "No collections in list.");
+        assertSucceeded(res, 'Failed to list collections.');
+        assert(res.body.list.length >= 1, 'No collections in list.');
       })
   );
 });
 
-describe("GET /collections/all/public", () => {
-  it("Should return a list with all public collections", () =>
+describe('GET /collections/all/public', () => {
+  it('Should return a list with all public collections', () =>
     request(app)
-      .get("/collections/all/public")
+      .get('/collections/all/public')
       .send()
       .then((res) => {
-        assertSucceeded(res, "Failed to list public collections.");
-        assert(res.body.list.length >= 1, "No collections returned.");
+        assertSucceeded(res, 'Failed to list public collections.');
+        assert(res.body.list.length >= 1, 'No collections returned.');
         res.body.list.forEach((collection) =>
-          assert(collection.public, "Non-public collection returned.")
+          assert(collection.public, 'Non-public collection returned.')
         );
       }));
 });
 
-describe("GET /collections/:id", () => {
-  itShouldRequireAuthentication(
-    "/collections/" + TestCollections.Breakfast.id,
-    "get"
-  );
+describe('GET /collections/:id', () => {
+  itShouldRequireAuthentication('/collections/' + TestCollections.Breakfast.id, 'get');
 
-  whenLoggedInIt("Should work author user", (token) =>
+  whenLoggedInIt('Should work author user', (token) =>
     request(app)
-      .get("/collections/" + TestCollections.Breakfast.id)
-      .set("Authorization", token)
+      .get('/collections/' + TestCollections.Breakfast.id)
+      .set('Authorization', token)
       .send()
       .then((res) => {
-        assertSucceeded(res, "Failed to get recipe.");
+        assertSucceeded(res, 'Failed to get recipe.');
         assert(
           res.body.collection?._id == TestCollections.Breakfast.id,
           "Response didn't have collection."
@@ -104,28 +99,26 @@ describe("GET /collections/:id", () => {
     "Shouldn't work for other user",
     (token) =>
       request(app)
-        .get("/recipes/" + TestCollections.Breakfast.id)
-        .set("Authorization", token)
+        .get('/recipes/' + TestCollections.Breakfast.id)
+        .set('Authorization', token)
         .send()
-        .then((res) =>
-          assertFailed(res, "Was able to access private collection.")
-        ),
+        .then((res) => assertFailed(res, 'Was able to access private collection.')),
     TestUsers.Beatrice
   );
 });
 
-describe("POST /collections/:id/add", () => {
+describe('POST /collections/:id/add', () => {
   // Must be function as id is not set initially.
-  const href = () => "/collections/" + TestCollections.Breakfast.id + "/add";
+  const href = () => '/collections/' + TestCollections.Breakfast.id + '/add';
 
   itShouldRequireAuthentication(href());
 
-  whenLoggedInIt("Should reject invalid recipe IDs", (token) =>
+  whenLoggedInIt('Should reject invalid recipe IDs', (token) =>
     request(app)
       .post(href())
-      .set("Authorization", token)
-      .send({ recipes: ["An Invalid ObjectID"] })
-      .then((res) => assertFailed(res, "Invalid recipe ID accepted."))
+      .set('Authorization', token)
+      .send({ recipes: ['An Invalid ObjectID'] })
+      .then((res) => assertFailed(res, 'Invalid recipe ID accepted.'))
   );
 
   // Must be function as id is not set initially.
@@ -136,22 +129,22 @@ describe("POST /collections/:id/add", () => {
     (token) =>
       request(app)
         .post(href())
-        .set("Authorization", token)
+        .set('Authorization', token)
         .send(body())
-        .then((res) => assertFailed(res, "Non-author user allowed to remove.")),
+        .then((res) => assertFailed(res, 'Non-author user allowed to remove.')),
     TestUsers.Beatrice
   );
 
-  whenLoggedInIt("Should work for author user", (token) =>
+  whenLoggedInIt('Should work for author user', (token) =>
     request(app)
       .post(href())
-      .set("Authorization", token)
+      .set('Authorization', token)
       .send(body())
       .then(async (res) => {
-        assertSucceeded(res, "Adding recipe not successful.");
+        assertSucceeded(res, 'Adding recipe not successful.');
         await request(app)
-          .get("/collections/" + TestCollections.Breakfast.id)
-          .set("Authorization", token)
+          .get('/collections/' + TestCollections.Breakfast.id)
+          .set('Authorization', token)
           .send()
           .then((res) => {
             assert(
@@ -163,18 +156,18 @@ describe("POST /collections/:id/add", () => {
   );
 });
 
-describe("POST /collections/:id/remove", () => {
+describe('POST /collections/:id/remove', () => {
   // Must be function as id is not set initially.
-  const href = () => "/collections/" + TestCollections.Breakfast.id + "/remove";
+  const href = () => '/collections/' + TestCollections.Breakfast.id + '/remove';
 
   itShouldRequireAuthentication(href());
 
-  whenLoggedInIt("Should reject invalid recipe IDs", (token) =>
+  whenLoggedInIt('Should reject invalid recipe IDs', (token) =>
     request(app)
       .post(href())
-      .set("Authorization", token)
-      .send({ recipes: ["An Invalid ObjectID"] })
-      .then((res) => assertFailed(res, "Invalid recipe ID accepted."))
+      .set('Authorization', token)
+      .send({ recipes: ['An Invalid ObjectID'] })
+      .then((res) => assertFailed(res, 'Invalid recipe ID accepted.'))
   );
 
   // Must be function as id is not set initially.
@@ -185,77 +178,74 @@ describe("POST /collections/:id/remove", () => {
     (token) =>
       request(app)
         .post(href())
-        .set("Authorization", token)
+        .set('Authorization', token)
         .send(body())
-        .then((res) => assertFailed(res, "Non-author user allowed.")),
+        .then((res) => assertFailed(res, 'Non-author user allowed.')),
     TestUsers.Beatrice
   );
 
-  whenLoggedInIt("Should work for author user", (token) =>
+  whenLoggedInIt('Should work for author user', (token) =>
     request(app)
       .post(href())
-      .set("Authorization", token)
+      .set('Authorization', token)
       .send(body())
       .then(async (res) => {
-        assertSucceeded(res, "Removing recipe not successful.");
+        assertSucceeded(res, 'Removing recipe not successful.');
         await request(app)
-          .get("/collections/" + TestCollections.Breakfast.id)
-          .set("Authorization", token)
+          .get('/collections/' + TestCollections.Breakfast.id)
+          .set('Authorization', token)
           .send()
           .then((res) => {
             assert(
               !res.body.collection.recipes.includes(TestRecipes.Rice.id),
-              "Recipe still in collection after removal."
+              'Recipe still in collection after removal.'
             );
           });
       })
   );
 });
 
-describe("PATCH /collections/:id", () => {
-  const href = () => "/collections/" + TestCollections.Breakfast.id;
+describe('PATCH /collections/:id', () => {
+  const href = () => '/collections/' + TestCollections.Breakfast.id;
 
   const patch = {
-    name: "Quick Sweets",
-    tags: ["quick", "easy"],
+    name: 'Quick Sweets',
+    tags: ['quick', 'easy']
   };
 
-  itShouldRequireAuthentication(href(), "patch");
+  itShouldRequireAuthentication(href(), 'patch');
 
   whenLoggedInIt(
-    "Should fail for non-author user",
+    'Should fail for non-author user',
     (token) =>
       request(app)
         .patch(href())
-        .set("Authorization", token)
+        .set('Authorization', token)
         .send(patch)
-        .then((res) => assertFailed(res, "Non-author allowed to update.")),
+        .then((res) => assertFailed(res, 'Non-author allowed to update.')),
     TestUsers.Beatrice
   );
 
-  whenLoggedInIt("Should work for author user", (token) =>
+  whenLoggedInIt('Should work for author user', (token) =>
     request(app)
       .patch(href())
-      .set("Authorization", token)
+      .set('Authorization', token)
       .send(patch)
       .then(async (res) => {
-        assertSucceeded(res, "Author not able to update.");
+        assertSucceeded(res, 'Author not able to update.');
         await request(app)
           .get(href())
-          .set("Authorization", token)
+          .set('Authorization', token)
           .send()
           .then((res) => {
-            assertSucceeded(res, "Updated collection missing.");
+            assertSucceeded(res, 'Updated collection missing.');
 
-            let collection = res.body.collection;
-            assert(collection.name == patch.name, "Name not updated.");
-            assert(
-              !collection.tags.includes("breakfast"),
-              "Tags not updated correctly."
-            );
+            const collection = res.body.collection;
+            assert(collection.name == patch.name, 'Name not updated.');
+            assert(!collection.tags.includes('breakfast'), 'Tags not updated correctly.');
             assert(
               collection.public == TestCollections.Breakfast.public,
-              "Publicity updated erroneously."
+              'Publicity updated erroneously.'
             );
           });
       })
@@ -263,34 +253,34 @@ describe("PATCH /collections/:id", () => {
 });
 
 // This test is last because it deletes the test record
-describe("DELETE /collections/:id", () => {
-  const href = () => "/collections/" + TestCollections.Breakfast.id;
+describe('DELETE /collections/:id', () => {
+  const href = () => '/collections/' + TestCollections.Breakfast.id;
 
-  itShouldRequireAuthentication(href(), "delete");
+  itShouldRequireAuthentication(href(), 'delete');
 
   whenLoggedInIt(
     "Shouldn't work for non-author user",
     (token) =>
       request(app)
         .delete(href())
-        .set("Authorization", token)
+        .set('Authorization', token)
         .send()
-        .then((res) => assertFailed(res, "Non-author user allowed.")),
+        .then((res) => assertFailed(res, 'Non-author user allowed.')),
     TestUsers.Beatrice
   );
 
-  whenLoggedInIt("Should work for author user", (token) =>
+  whenLoggedInIt('Should work for author user', (token) =>
     request(app)
       .delete(href())
-      .set("Authorization", token)
+      .set('Authorization', token)
       .send()
       .then(async (res) => {
-        assertSucceeded(res, "Deleting collection not successful.");
+        assertSucceeded(res, 'Deleting collection not successful.');
         await request(app)
           .get(href())
-          .set("Authorization", token)
+          .set('Authorization', token)
           .send()
-          .then((res) => assertFailed(res, "Collection not deleted."));
+          .then((res) => assertFailed(res, 'Collection not deleted.'));
       })
   );
 });
