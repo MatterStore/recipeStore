cf4ad7cb7ee32f23b6eb5988fc990fd107888a0d
manu354 <mmasson@student.unimelb.edu.au>
2022-09-27 18:55:58 +1000

    MS-93:fixups

--- backend/config/db-connection.ts
+++ backend/config/db-connection.ts
@@ -6,10 +6,7 @@ import { MongoMemoryServer } from 'mongodb-memory-server';
 
 const DB_USER = process.env.DB_USER;
 const DB_PASSWORD = process.env.DB_PASSWORD;
-const DB_HOST =
-  process.env.NODE_ENV == 'testing'
-    ? 'cluster0.rvya9b7.mongodb.net'
-    : 'cluster0.jd0lpeu.mongodb.net';
+const DB_HOST = 'cluster0.jd0lpeu.mongodb.net';
 const DB_URL =
   `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}` +
   '/?retryWrites=true&w=majority';
