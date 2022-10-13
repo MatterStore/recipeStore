0aada9e9d0ff5bb1a0e6733cf0eceb0510d2f7d3
Paul Hutchins <paul.hutchins@uptickhq.com>
2022-10-14 00:23:03 +1100

    Feat: Added ability to delete a recipe

--- frontend/src/api/routes.js
+++ frontend/src/api/routes.js
@@ -1,6 +1,7 @@
 export const loginRoute = '/user/login';
 export const signupRoute = '/user/signup';
 export const allRecipesRoute = '/recipes/all/public';
+export const listingRoute = '/listing';
 export const recipesRoute = (id) => `/recipes/${id}/`;
 export const newRecipeRoute = `/recipes/new/`;
 export const changePasswordRoute = '/user/update-password';
