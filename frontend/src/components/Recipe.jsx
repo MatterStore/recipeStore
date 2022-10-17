c52a0bf84823ede47437a90f78510f4d96435ca4
OwenFeik <owenhfeik@gmail.com>
2022-10-17 14:54:58 +1100

    MS-124: Fall back to first image if prop missing

--- frontend/src/components/Recipe.jsx
+++ frontend/src/components/Recipe.jsx
@@ -1,13 +1,15 @@
 import { Link } from 'react-router-dom';
 
 export default function Recipe(props) {
+  let primaryImage = props.primaryImage || props?.images?.[0];
+
   return (
     <Link
       to={`/recipe/${props._id}`}
       className={`w-96 h-64 bg-gray-200 mr-4 mb-4 rounded p-8 inline-block text-xl`}>
       {props.title}
       <img
-        src={props.primaryImage}
+        src={primaryImage}
         alt=""
         className="object-cover w-full max-h-full pb-12 mt-4 rounded"
       />
