f7292852a6c353c32768f2529606da1c0883a67f
Daniel B <dantheman41@live.com.au>
2022-10-23 17:44:39 +1100

    MS-128: Click callback test

--- frontend/src/components/__tests__/ButtonTest.js
+++ frontend/src/components/__tests__/ButtonTest.js
@@ -1,5 +1,5 @@
 import Button from '../Button'
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import { MemoryRouter } from 'react-router-dom';
 
 describe("Button tests", () => {
@@ -15,5 +15,22 @@ describe("Button tests", () => {
             </MemoryRouter>
         );
         screen.getByText("Test Button").classList.contains("custom-class")
+    });
+
+    it ("should call click handler", () => {
+        const callback = jest.fn()
+        render(
+            <MemoryRouter>
+                <Button
+                    className="custom-class"
+                    to="test"
+                    onClick={callback}
+                >
+                    Test Button
+                </Button>
+            </MemoryRouter>
+        );
+        fireEvent.click(screen.getByText("Test Button"));
+        expect(callback.mock.calls.length).toBe(1);
     });
 });
